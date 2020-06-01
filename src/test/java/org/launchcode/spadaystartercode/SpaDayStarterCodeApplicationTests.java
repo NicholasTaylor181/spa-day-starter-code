6b3816de9059e160a46014218d71d78b546f1d96
Nick <nicholastaylor181@gmail.com>
2020-06-01 18:17:20 -0500

    finished studio

---
+++ src/test/java/org/launchcode/spadaystartercode/SpaDayStarterCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.spadaystartercode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaDayStarterCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
